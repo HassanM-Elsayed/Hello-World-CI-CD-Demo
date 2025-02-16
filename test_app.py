b8fce4f38be4fcdfff91a6360c7dae2dccbe7a84
Hassan <hassan.telep@neweratech.com>
2025-02-16 08:54:21 +0400

    Add test for  Hello world App

---
+++ test_app.py
@@ -0,0 +1,8 @@
+import unittest from app import hello_world
+
+class Testapp(unittest.TestCase):
+  def test_hello_world(self);
+   self.assertEqual(hello_world(),"Hello World")
+
+if_name==="_main_":
+    unittest()
